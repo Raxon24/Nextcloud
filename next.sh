a1fffd14fdc874c3ebe69f974c69b4e408379805
Rax <61168967+Raxon24@users.noreply.github.com>
2023-11-23 19:17:11 -0500

    Create next.sh

---
+++ next.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ bold=`echo -en "\e[1m"`
+ blink=`echo -en "\e[5m"`
+ normal=`echo -en "\e[0m"`
+ green=`echo -en "\e[32m"`
+ blue=`echo -en "\e[34m"`
+ lightyellow=`echo -en "\e[93m"`
+ DEFAULT=`echo -en "\e[49m"`
+
+clear
+
+echo "${green}${bold}      DATA SAVED"
+sleep 2
+echo "$normal"
+echo "${lightyellow}  setting up to start"
+echo "$normal"
+echo "${blue}${bold}${blink}      NEXTCLOUD"
+echo "$normal"
+echo "${lightyellow}     instalation"
+
+echo "$normal"
